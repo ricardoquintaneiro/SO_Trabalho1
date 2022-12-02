d754ab15737955322529e05898a7d1493088c391
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2022-12-02 21:28:55 +0000

    Versão final

--- rwstat.sh
+++ rwstat.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Aqui, em vez de declararmos arrays para read e write, podemos declarar uma matrix, em que cada linha é um processo e cada coluna é uma informação relativa ao processo
-
-export LANG=en_US.UTF-8
-export LANGUAGE=en
-declare -A matrix
-column=6
-reverse=""
-
+#Declaração de matriz e valores DEFAULT
+export LANG=en_US.UTF-8 # Formatação do printf e da data
+export LANGUAGE=en # Muda a linguagem da bash
+declare -A matrix       # Matriz 2D com informações sobre os processos
+column=6                # Valor DEFAULT da coluna de ordenação da matriz
+reverse=""              # Valor DEFAULT da direção de ordenação
+regex_positive_int='^[0-9]+$'   # Regex para verificação numérica
 
 # Mensagem que demonstra quais opções podem ser utilizadas ao executar este ficheiro. Esta mensagem aparece quando os argumentos de entrada não estão bem formatados.
 usage() { echo "Usage: $0 [-c <regex>] [-s <mindate>] [-e <maxdate>] [-u <user>]\
  [-m <minPID>] [-M <maxPID>] [-p <num>] [-r] [-w] <sleepT>" 1>&2; exit 1; }
 
-# VP - Verificação de Processos
-
 regex_positive_int='^[0-9]+$'
+
+# Validação de argumentos
 
 while getopts "c:s:e:u:m:M:p:rw" o; do
     case "${o}" in
@@ -74,7 +73,8 @@ if [ -z "$1" ] || ! [ -z "$2" ] ||
 fi
 sleep_time=$1
 
-# LRP - Leitura e Registo de Processos
+# Primeira Leitura
+
 i=1
 while IFS= read -r line
 do
@@ -109,7 +109,7 @@ do
         fi
         date_readable=$(date -d @$date_epoch +%b' '%e' '%k:%M)
 
-        # Aqui no fim, podemos registar a informação que já temos (nome, data, user) e guardar num array ou num string, para termos menos trabalho no fim
+        # No fim, a informação que já temos (nome, data, user) é guardada num array ou num string, para termos menos trabalho no fim
 
         matrix[$i,1]=$comm
         matrix[$i,2]=$user
@@ -124,7 +124,7 @@ done < <(ls -l /proc/ | awk '{print $9}' | grep -x -E '[0-9]+')
 # Este sleep tem que ser feito para podermos calcular o rateR e o rateW, que necessitam de uma medição de tempo
 sleep $sleep_time
 
-# OID - Ordenação de Informação para Display
+# Segunda Leitura
 
 for ((j=1;j<$i;j++)) do
         cat /proc/${matrix[$j,3]}/comm &> /dev/null
@@ -140,7 +140,7 @@ for ((j=1;j<$i;j++)) do
 
 done
 
-# DI - Display de Informação
+# Display de Informação
 
 printf "%-20s%-9s%6s%11s%12s%12s%14s%14s\n" "COMM" "USER" "PID" "READB" "WRITEB" "RATER"\
         "RATEW" "DATE"
@@ -150,8 +150,11 @@ for ((j=1;j<$i;j++)) do
         if ! [ $? == 0 ]; then
                 continue
         fi
-        printf "%-20s \b%-9s \b%6s \b%11s \b%12s \b%12s \b%14s \b%14s\n" "${matrix[$j,1]}" "${matrix[$j,2]}" "${matrix[$j,3]}" "${matrix[$j,4]}" "${matrix[$j,5]}"\
-                "${matrix[$j,6]}" "${matrix[$j,7]}" "${matrix[$j,8]}"
+        printf "%-20s \b%-9s \b%6s \b%11s \b%12s \b%12s \b%14s \b%14s\n" "${matrix[$j,1]}"\
+         "${matrix[$j,2]}" "${matrix[$j,3]}" "${matrix[$j,4]}" "${matrix[$j,5]}" "${matrix[$j,6]}"\
+          "${matrix[$j,7]}" "${matrix[$j,8]}"
 done |
  sort -t $'\b' -"$reverse"g -k"$column" |
   ( ! [ -z "$p" ] && awk "NR<=$p" || awk "NR>0")
+
+# Os valores finais de readb e writeb correspondem aos valores de leitura e escrita durante o tempo estipulado.
